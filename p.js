843f721103c9dea100f9e832404bcb8f0bf983c6
adria tavares <ats19@aluno.ifal.edu.br>
2026-06-10 23:55:31 -0300

    consultas básicas do dao prontas

--- p.js
+++ p.js
@@ -1,3 +1,58 @@
+async function getQuartosDisponiveis(data) {
+	let filtros = [];
+	let valores = [];
+	if (data.id_tipo_quarto !== undefined) {
+		filtros.push('AND q.id_tipo_quarto = ?');
+		valores.push(data.id_tipo_quarto);
+	}
+	if (data.inicio !== undefined) {
+	}
+
+	valores.push(data.fim);
+	valores.push(data.inicio);
+
+	const sql = `
+        SELECT
+            q.id,
+            q.numero,
+            q.andar,
+            q.estado,
+            tq.id AS id_tipo_quarto,
+            tq.nome AS tipo_quarto,
+            tq.capacidade,
+            tq.descricao,
+            tq.valor_diaria,
+            (
+				SELECT
+					JSON_ARRAYAGG(
+						JSON_OBJECT(
+							'inicio',r.inicio,
+							'fim', r.fim
+						)
+					)
+                )
+                FROM reservas r
+                WHERE
+                    r.id_quarto = q.id AND
+                    r.estado = 'confirmada'
+                ORDER BY r.inicio
+                GROUP BY r.id_quarto
+
+                ) as intervalos_ocupados
+        FROM quartos AS q
+
+        INNER JOIN tipos_quartos AS tq
+            ON tq.id = q.id_tipo_quarto
+
+        WHERE q.estado = 'disponivel'
+          AND tq.ativo = TRUE
+          ${filtroTipo}
+    `;
+
+	const [rows] = await this.DB.execute(sql, valores);
+	return rows;
+}
+
 let R = [
 	{ inicio: '2000-01-01T00:00:00.000Z', fim: '2000-01-07T00:00:00.000Z' },
 	{ inicio: '2000-01-10T00:00:00.000Z', fim: '2000-01-11T00:00:00.000Z' },
