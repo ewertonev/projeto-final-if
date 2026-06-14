06a891d95802691324f7f4d32e77a9a42be3ee83
Ewerton Evaristo <ees4@aluno.ifal.edu.br>
2026-06-14 19:13:20 -0300

    Atualiza server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(cors())
 
 
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+app.listen(3000, () => console.log('Servidor rodando na porta 3000, http://localhost:3000'))
