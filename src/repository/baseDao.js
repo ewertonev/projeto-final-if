731511eb8540488c844d5a52d27fc9e7a96f1a04
adria tavares <ats19@aluno.ifal.edu.br>
2026-06-14 19:31:27 -0300

    deleção de arquivos e pastas antigos

--- src/repository/baseDao.js
+++
@@ -1,7 +0,0 @@
-import pool from '../../database/conexao.js';
-export class BaseDao {
-    DB;
-    constructor() {
-        this.DB = pool;
-    }
-}
--- src/services/usuario.js
+++
@@ -1,6 +0,0 @@
-import { UsuariosDao } from '../repository/usuarioDao.js';
-import cpf from '../validators/cpf.js';
-import email from '../validators/email.js';
-import telefone from '../validators/telefone.js';
-
-class UsuarioService {}
